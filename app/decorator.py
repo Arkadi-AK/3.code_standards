1ce7c45661c52f4f4fcf1f857fda79eea4994f60
Arkadi-AK <akpy375@gmail.com>
2023-01-03 12:52:39 +0300

    change file pyrpoject.toml

--- app/decorator.py
+++ app/decorator.py
@@ -1,23 +1,23 @@
-import time
+import time, os
 
 
 def repeater(call_count, start_sleep_time, factor, border_sleep_time):
     def outer(fnc):
         def inner():
             if call_count == 0:
-                return print(f'Запусков не будет')
+                return print(f"Запусков не будет")
             else:
-                print(f'Кол-во запусков = {call_count}')
-                print('Начало работы')
+                print(f"Кол-во запусков = {call_count}")
+                print("Начало работы")
                 for n in range(call_count):
-                    t = start_sleep_time * factor ** n
+                    t = start_sleep_time * factor**n
                     if t >= border_sleep_time:
                         t = border_sleep_time
-                    print(f'Запуск номер {n + 1}. Ожидание: {t} секунд.', end=' ')
+                    print(f"Запуск номер {n + 1}. Ожидание: {t} секунд.", end=" ")
                     time.sleep(t)
                     func_result = fnc()
-                    print(f'Результат декорируемой функций = {func_result}.')
-                print('Конец работы')
+                    print(f"Результат декорируемой функций = {func_result}.")
+                print("Конец работы")
 
         return inner
 
@@ -26,8 +26,9 @@ def repeater(call_count, start_sleep_time, factor, border_sleep_time):
 
 @repeater(call_count=4, start_sleep_time=1, factor=2, border_sleep_time=55)
 def func():
-    return 'Hello, admin'
+
+    return "Hello, admin"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     func()
