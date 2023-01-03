20bfb943e754d0073f5549bde81a3649f1c466b2
Arkadi-AK <akpy375@gmail.com>
2023-01-03 13:43:24 +0300

    refactoring decorator.py

--- app/decorator.py
+++ app/decorator.py
@@ -1,23 +1,25 @@
-import time, os
+"""Decorator with parameters"""
+import time
 
 
 def repeater(call_count, start_sleep_time, factor, border_sleep_time):
+    """Decorator repeater"""
+
     def outer(fnc):
         def inner():
             if call_count == 0:
-                return print(f"Запусков не будет")
-            else:
-                print(f"Кол-во запусков = {call_count}")
-                print("Начало работы")
-                for n in range(call_count):
-                    t = start_sleep_time * factor**n
-                    if t >= border_sleep_time:
-                        t = border_sleep_time
-                    print(f"Запуск номер {n + 1}. Ожидание: {t} секунд.", end=" ")
-                    time.sleep(t)
-                    func_result = fnc()
-                    print(f"Результат декорируемой функций = {func_result}.")
-                print("Конец работы")
+                return print("Запусков не будет")
+            print(f"Кол-во запусков = {call_count}")
+            print("Начало работы")
+            for number in range(call_count):
+                timer = start_sleep_time * factor ** number
+                timer = min(timer, border_sleep_time)
+                print(f"Запуск номер {number + 1}Ожидание: {timer} секунд.", end=" ")
+                time.sleep(timer)
+                func_result = fnc()
+                print(f"Результат декорируемой функций = {func_result}.")
+            print("Конец работы")
+            return None
 
         return inner
 
@@ -26,7 +28,7 @@ def repeater(call_count, start_sleep_time, factor, border_sleep_time):
 
 @repeater(call_count=4, start_sleep_time=1, factor=2, border_sleep_time=55)
 def func():
-
+    """The function that the decorator should use"""
     return "Hello, admin"
 
 
